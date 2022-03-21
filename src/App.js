3c689d475b58dbbed324720520a565e50e91b633
Nayeon Shin <nayeon.codes@gmail.com>
2022-03-21 00:04:11 -0400

    refactor: Create `<Converter />`

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import { useEffect, useState } from "react";
 
+const Converter = ({ coins }) => {
+  return (
+    <select>
+      {coins.map((coin) => (
+        <option key={coin.id}>
+          {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} (USD)
+        </option>
+      ))}
+    </select>
+  );
+};
+
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [coins, setCoins] = useState([]);
@@ -19,17 +31,7 @@ function App() {
   return (
     <div>
       <h1>Coin Tracker {isLoading ? "" : `(${coins.length})`}</h1>
-      {isLoading ? (
-        <h3>Loading...</h3>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} (USD)
-            </option>
-          ))}
-        </select>
-      )}
+      {isLoading ? <h3>Loading...</h3> : <Converter coins={coins} />}
     </div>
   );
 }
