e6b9a969c73c4d6db6c4ddf5f385223e31774773
Nayeon Shin <nayeon.codes@gmail.com>
2022-03-21 01:59:13 -0400

    refactor: Create `Converter.js`

---
+++ src/Converter.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+const Converter = ({ coins }) => {
+  const [amount, setAmount] = useState(0);
+  const [value, setValue] = useState(0);
+
+  const onChange = (event) => setAmount(event.target.value);
+  const onSelect = (event) => setValue(event.target.value);
+
+  const {
+    symbol,
+    quotes: {
+      USD: { price },
+    },
+  } = coins[value];
+
+  return (
+    <>
+      <input
+        onChange={onChange}
+        placeholder="Enter USD here."
+        type="number"
+        value={amount}
+      />
+      <span style={{ marginLeft: "0.5em" }}>USD</span>
+      <h3>
+        ~{Math.round(amount / price)} {symbol}
+      </h3>
+      <select onChange={onSelect}>
+        {coins.map((coin, index) => (
+          <option key={coin.id} value={index}>
+            {coin.name} ({symbol}): ${price} (USD)
+          </option>
+        ))}
+      </select>
+    </>
+  );
+};
+
+export default Converter;
