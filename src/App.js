b2e93f4e865a3fb491a78395f942893046e192a2
Nayeon Shin <nayeon.codes@gmail.com>
2022-03-21 01:34:43 -0400

    feat: Complete implementing `<Converter />`

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import { useEffect, useState } from "react";
 
 const Converter = ({ coins }) => {
+  const [amount, setAmount] = useState(0);
+  const [value, setValue] = useState(0);
+
+  const onChange = (event) => setAmount(event.target.value);
+  const onSelect = (event) => setValue(event.target.value);
+
+  const {
+    symbol,
+    quotes: {
+      USD: { price },
+    },
+  } = coins[value];
+
   return (
-    <select>
-      {coins.map((coin) => (
-        <option key={coin.id}>
-          {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} (USD)
-        </option>
-      ))}
-    </select>
+    <>
+      <input
+        onChange={onChange}
+        placeholder="Enter USD here."
+        type="number"
+        value={amount}
+      />
+      <span style={{ marginLeft: "0.5em" }}>USD</span>
+      <h3>
+        ~{Math.round(amount / price)} {symbol}
+      </h3>
+      <select onChange={onSelect}>
+        {coins.map((coin, index) => (
+          <option key={coin.id} value={index}>
+            {coin.name} ({symbol}): ${price} (USD)
+          </option>
+        ))}
+      </select>
+    </>
   );
 };
 
@@ -30,7 +55,7 @@ function App() {
 
   return (
     <div>
-      <h1>Coin Tracker {isLoading ? "" : `(${coins.length})`}</h1>
+      <h1>USD to Coin {isLoading ? "" : `(${coins.length} coins)`}</h1>
       {isLoading ? <h3>Loading...</h3> : <Converter coins={coins} />}
     </div>
   );
